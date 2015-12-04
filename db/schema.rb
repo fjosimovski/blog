2e472bf252236017f95b3bf90bdcb4d72acfe818
Filip Josimovski <josimovski.filip@gmail.com>
2015-12-04 12:25:27 +0100

    Created user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204105830) do
+ActiveRecord::Schema.define(version: 20151204112503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20151204105830) do
     t.string   "title"
     t.string   "description"
     t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
